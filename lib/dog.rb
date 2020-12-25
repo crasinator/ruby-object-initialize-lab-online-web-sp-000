8ed57a9e57dc411ae362c866b97cb5c0684b81a6
Jordan Reed <jordanreed03@gmail.com>
2020-12-25 16:25:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
-def intialize (dogs_name, breed = "mutt")
-  @name = dogs_name
-  @breed = breed 
-end 
+  def intialize (dogs_name, breed = "mutt")
+    @name = dogs_name
+    @breed = breed 
+  end 
 end 
